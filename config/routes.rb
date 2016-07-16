548de22d636f2f2afcc31b8d4e9c0d7c854aa86b
PierreDV <p.verteuil@gmail.com>
2016-07-16 15:18:08 -0400

    added links for login/logout and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  resources :welcome
 
   root 'welcome#index'
 
-  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
-  resources :sessions, only: [:new, :create]
+  resources :welcome
+
+  resources :users, only: [:new, :create]
 
 end
