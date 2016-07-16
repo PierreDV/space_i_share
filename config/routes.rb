3423bd16fa012a09a308b7ea1e9520f0835e6859
PierreDV <p.verteuil@gmail.com>
2016-07-16 12:24:59 -0400

    Setup basic scaffolding for the user-model, controller as well as the initial schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'users/new'
+
+  get 'users/create'
+
+  resources :users, only: [:new, :create, :destroy]
+
 end
