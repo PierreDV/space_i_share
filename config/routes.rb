768ddbe1592b4489aaf214ed74f272b4951c87df
PierreDV <p.verteuil@gmail.com>
2016-07-16 14:55:59 -0400

    added login functionality, users will now create sessions when they log on

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   resources :welcome
 
   root 'welcome#index'
 
-  resources :users, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create]
 
 end
