74327a53b504ba378d5ed17ce913a750aebaf72e
PierreDV <p.verteuil@gmail.com>
2016-07-16 12:55:17 -0400

    added welcome page and updated the user controller to handle user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to "../views/welcome/index.html.erb"
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
