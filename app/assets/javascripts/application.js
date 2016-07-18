1eecf3705d8297bd7fd870b715f62450014fcd4b
PierreDV <p.verteuil@gmail.com>
2016-07-17 21:07:48 -0400

    expanded on my 'square-creator' function so tht it will produce 4 randomly colored divs within the parent div

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-require './squares.js'
+
+  // require './squares.js'
--- app/assets/javascripts/squares.js
+++ app/assets/javascripts/squares.js
@@ -1,4 +1,12 @@
 function squarePusher(event) {
-  const box1 = document.createElement('div');
-  event.target.appendChild(box1);
+  for(i=0; i<4; i++) {
+    var box1 = document.createElement('div');
+    box1.className = 'square';
+    box1.style.backgroundColor = randomColor();
+    event.target.appendChild(box1);
+  }
+}
+
+function randomColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
